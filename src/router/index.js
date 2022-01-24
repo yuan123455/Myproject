4990f9e946c5da658c5ebc0f56e58ebe01276003
盛夏 <2847349116@qq.com>
2022-01-24 22:30:56 +0800

    添加主体架构

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,34 @@ import Vue from "vue";
 Vue.use(VueRouter)
 
 export default new VueRouter({
-    routes:[
+    routes: [
+        {
+            path: "/",
+            name: "index",
+            component: () => import("@/views/index.vue"),
+            children: [
+                {
+                    path: "/",
+                    name: 'mybook',
+                    component: () => import("@/views/mybook.vue"),
+                },
+                {
+                    path: "notes",
+                    name: 'notes',
+                    component: () => import("@/views/notes.vue"),
+                },
+                {
+                    path: "photo",
+                    name: 'photo',
+                    component: () => import("@/views/photo.vue"),
+                },
+                {
+                    path: "record",
+                    name: 'record',
+                    component: () => import("@/views/record.vue"),
+                }
+            ],
+        },
+
     ]
 })
